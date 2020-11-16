191f4477ed3fd28c005aa523be1e7db325aa52e7
gitduk <wdkany@gmail.com>
2020-11-17 03:50:38 +0800

    ...

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def index():
+    doc_file_name_list = os.listdir(r"./templates/documents")
+    doc_list = [file_name.replace('.html', '') for file_name in doc_file_name_list]
+    return render_template("index.html", **locals())
+
+
+@app.route('/documents/<doc_name>')
+def doc(doc_name):
+    return render_template(f"documents/{doc_name}.html")
 
 
 if __name__ == '__main__':
---
+++ markdown/Flask.md
@@ -0,0 +1,29 @@
+## Flask
+
+#### Flask 简略运行过程
+
+- flask 应用实例会启动一个微型服务器
+- 微型服务器收集用户请求用 `Werkzeug` 做路由分发
+- 根据路由找到对应的路由函数，返回数据给用户
+- 返回的数据可以是字符串和 html 模板
+
+
+
+#### 创建 `app` 实例，并启动微型服务器
+
+```python
+from flask import Flask
+
+# create app
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "this is a route function"
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port="8080")
+```
+
+
+
